1ce17a16636df1855cbe7d8a1f0437343456d78c
jacob-s-codes <jacobbshaul@gmail.com>
2025-03-20 18:38:46 -0700

    see if this fixes issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
           <div className="relative max-w-7xl flex flex-col h-screen mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
             <h1 className="text-4xl font-black tracking-tight text-white sm:text-5xl lg:text-9xl"><span className='text-darkred'>UHS</span> Hacks 2025</h1>
             <p className="mt-6 max-w-3xl font-semibold text-2xl text-white">
-              Join us for 48 hours of coding, learning, and innovation at our school's premier hackathon event.
+              Join us for 48 hours of coding, learning, and innovation at our schools premier hackathon event.
             </p>
             <div className="mt-10 flex flex-col sm:flex-row gap-4">
               <div className="relative inline-flex  group">
