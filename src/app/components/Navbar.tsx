467ec17268b20bbcf3b5eae4a643b89b0852320c
jacob-s-codes <jacobbshaul@gmail.com>
2025-03-28 21:16:06 -0700

    Changed accordion

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import { useState } from 'react';
 import Registerbtn from './Registerbtn';
