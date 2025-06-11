ff99bd36a7556cf553601d08908230594a63a08c
jacob-s-codes <jacobbshaul@gmail.com>
2025-06-11 14:48:20 -0700

    added cerebras logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -315,9 +315,10 @@ export default function Home() {
         <div className='max-w-7xl mx-auto'>
           <h2 className='text-center lg:text-5xl text-4xl font-black'>THANK YOU TO OUR SPONSORS</h2>
           <hr className='mb-8 mt-1 border-2 border-black' />
-          <div className='grid grid-cols-1 md:grid-cols-2 gap-8 place-items-center'>
-            <a href="https://modetocode.com" target='_blank' className='hover:scale-105 hover:rotate-3 duration-200 '><img src="/mtclogo.png" alt="Mode to Code sponsor" className='rounded-full w-auto h-96' /></a>
-            <a href="https://www.sfuhs.org/" target='_blank' className='hover:scale-105 hover:rotate-3 duration-200 w-96 h-96'><img src="/uhslogo.jpg" alt="Mode to Code sponsor" className='rounded-full w-96 h-96' /></a>
+          <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 place-items-center'>
+            <a href="https://modetocode.com" target='_blank'><img src="/mtclogo.png" alt="Mode to Code sponsor" className='rounded-full w-auto h-72 hover:scale-105 hover:rotate-3 duration-200 ' /></a>
+            <a href="https://www.sfuhs.org/" target='_blank'><img src="/uhslogo.jpg" alt="Mode to Code sponsor" className='rounded-full hover:scale-105 hover:rotate-3 duration-200 w-auto h-72' /></a>
+            <a href="https://www.cerebras.ai/" target='_blank'><img src="/cerebraslogo.avif" alt="Cerebras sponsor" className='hover:scale-105 hover:rotate-3 duration-200 w-auto h-72 border'/></a>
             <p className='text-xl text-center '>Interested in sponsoring UHS Hacks? Email: <a href="mailto:info@uhshacks.com" className='font-bold hover:cursor-pointer'>info@uhshacks.com</a></p>
           </div>
 
