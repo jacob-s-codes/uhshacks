06a95d65d15b12620d094f739419a69499bc0b9d
jacob-s-codes <jacobbshaul@gmail.com>
2025-07-22 22:21:33 -0700

    added custom 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+    return (
+        <div className="pt-12 text-center">
+            <h1 className="font-bold text-3xl">Page not found</h1>
+            <h2 className="pt-4">Think this is a mistake? Email: <a href="mailto:info@uhshacks.com" className="text-darkred hover:underline">info@uhshacks.com</a></h2>
+        </div>
+    )
+}
