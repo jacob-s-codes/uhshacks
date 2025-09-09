389215ff99e144687d4f79956f936945c4086108
jacob-s-codes <jacobbshaul@gmail.com>
2025-09-09 16:34:09 -0700

    fixed discord

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -72,7 +72,7 @@ const HomeClient = () => {
               <div
                 className="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-darkred via-red-400 to-red-800 rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
               </div>
-              <a href="https://discord.gg/69DBGBKeGz" title="Join the discord" target='_blank'
+              <a href="https://discord.gg/Kz3RqUbEbt?event=1402169144304074813" title="Join the discord" target='_blank'
                 className="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-black transition-all duration-200 bg-white font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-gray-900"
                 role="button">Join The Discord
               </a>
@@ -455,7 +455,7 @@ const HomeClient = () => {
               </svg>
             </a>
 
-            <a href="https://discord.gg/69DBGBKeGz" target="_blank" className=" hover:text-black">
+            <a href="https://discord.gg/Kz3RqUbEbt?event=1402169144304074813" target="_blank" className=" hover:text-black">
               <span className="sr-only">Discord</span>
               <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-discord w-8 h-auto hover:cursor-pointer" viewBox="0 0 16 16">
                 <path d="M13.545 2.907a13.2 13.2 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.2 12.2 0 0 0-3.658 0 8 8 0 0 0-.412-.833.05.05 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.04.04 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032q.003.022.021.037a13.3 13.3 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019q.463-.63.818-1.329a.05.05 0 0 0-.01-.059l-.018-.011a9 9 0 0 1-1.248-.595.05.05 0 0 1-.02-.066l.015-.019q.127-.095.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.05.05 0 0 1 .053.007q.121.1.248.195a.05.05 0 0 1-.004.085 8 8 0 0 1-1.249.594.05.05 0 0 0-.03.03.05.05 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.2 13.2 0 0 0 4.001-2.02.05.05 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.03.03 0 0 0-.02-.019m-8.198 7.307c-.789 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612m5.316 0c-.788 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612" />
