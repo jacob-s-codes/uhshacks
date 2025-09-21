3299c4740d2a17f1a0e1d0758c742160a5debcf0
jacob-s-codes <jacobbshaul@gmail.com>
2025-09-20 21:56:01 -0700

    added images

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -5,6 +5,7 @@ import CryptoText from './components/CryptoTextOnce'
 import Registerbtn from './components/Registerbtn'
 import Judges from './components/Judge'
 import Person from './components/Person'
+import Carosel from './components/Carosel'
 
 const HomeClient = () => {
   return (
@@ -85,111 +86,13 @@ const HomeClient = () => {
       <div className="bg-gradient-to-b from-white to-red-200">
         <div id="about">
           <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-            <h2 className='text-center pt-36  lg:text-5xl text-4xl font-black pb-2'>LOGISTICAL INFORMATION</h2>
+            <h2 className='text-center pt-36  lg:text-5xl text-4xl font-black pb-2'>UHS HACKS 2025</h2>
             <hr className='mb-12 border-2 border-black' />
-            <div className="grid grid-cols-1 gap-8 md:grid-cols-3 ">
-              <div className="flex flex-col items-center">
-                <div className="rounded-md p-3">
-                  <svg className="h-18 w-auto text-darkred" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                  </svg>
-                </div>
-                <h3 className="mt-4 text-3xl font-medium ">September 20, 2025</h3>
-                <p className="mt-2 text-xl text-gray-700">Saturday</p>
-              </div>
-
-              <div className="flex flex-col items-center">
-                <div className="rounded-md  p-3">
-                  <svg className="h-18 w-auto text-darkred" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                  </svg>
-                </div>
-                <h3 className="mt-4 text-3xl font-medium">University High School</h3>
-                <Link href="https://maps.app.goo.gl/XBRvN4v2F6Wezd7E7" target='_blank'><p className="mt-2 text-xl text-gray-700 hover:text-darkred">3150 California Street | Directions here</p></Link>
-              </div>
-
-              <div className="flex flex-col items-center">
-                <div className="rounded-md  p-3">
-                  <svg className="h-18 w-auto text-darkred" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-                  </svg>
-                </div>
-                <h3 className="mt-4 text-3xl font-medium ">9 AM - 6:45 PM</h3>
-                <p className="mt-2 text-xl text-gray-700">Coding, learning, and fun</p>
-              </div>
-            </div>
+            <Carosel/>
           </div>
         </div>
 
-        {/* Feature Section */}
-        <div className=" py-36" >
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="text-center">
-              <h2 className="md:text-5xl text-3xl font-black text-black uppercase pb-2">Why Participate in UHS Hacks?</h2>
-              <hr className='border-2 border-black' />
-
-            </div>
-
-            <div className="mt-16">
-              <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-                <div className="bg-white overflow-hidden shadow-lg shadow-red-300 rounded-lg">
-                  <div className="px-4 py-5 sm:p-6">
-                    <div className="flex items-center">
-                      <div className="flex-shrink-0 bg-darkred rounded-md p-3">
-                        <svg className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-                        </svg>
-                      </div>
-                      <div className="ml-5">
-                        <h3 className="text-lg font-medium text-gray-900">Team Building</h3>
-                        <p className="mt-2 text-base text-gray-500">
-                          Form teams up to 4 students and collaborate on innovative projects
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="bg-white overflow-hidden shadow-lg shadow-red-300 rounded-lg ">
-                  <div className="px-4 py-5 sm:p-6">
-                    <div className="flex items-center">
-                      <div className="flex-shrink-0 bg-darkred rounded-md p-3">
-                        <svg className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
-                        </svg>
-                      </div>
-                      <div className="ml-5">
-                        <h3 className="text-lg font-medium text-gray-900">Innovate & Learn</h3>
-                        <p className="mt-2 text-base text-gray-500">
-                          Mentors help you learn new technologies and skills
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="bg-white overflow-hidden shadow-lg shadow-red-300 rounded-lg">
-                  <div className="px-4 py-5 sm:p-6">
-                    <div className="flex items-center">
-                      <div className="flex-shrink-0 bg-darkred rounded-md p-3">
-                        <svg className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg>
-                      </div>
-                      <div className="ml-5">
-                        <h3 className="text-lg font-medium text-gray-900">Win Prizes</h3>
-                        <p className="mt-2 text-base text-gray-500">
-                          $10,000+ in prizes for top projects/teams + free swag
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        
 
         {/* Schedule */}
         <div className='py-36 max-w-7xl mx-auto  px-4 sm:px-6 lg:px-8' id="schedule">
---
+++ src/app/components/Carosel.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Marquee from 'react-fast-marquee'
+
+const Carosel = () => {
+  return (
+    <div>
+        <Marquee className="max-h-[600px]  py-3 overflow-hidden grid  " speed={150} pauseOnHover={true}>
+            <img src="/2025/wideimg.jpg" alt="wide image" className='lg:h-[500px] md:h-96 h-72 flex lg:mx-10 mx-2'/>
+            <img src="/2025/winners.jpg" alt="wide image" className='lg:h-[500px] md:h-96 h-72 flex lg:mx-10 mx-2'/>
+            <img src="/2025/wideimg2.jpg" alt="wide image" className='lg:h-[500px] md:h-96 h-72 flex lg:mx-10 mx-2'/>
+            <img src="/2025/judging2.jpg" alt="wide image" className='lg:h-[500px] md:h-96 h-72 flex lg:mx-10 mx-2'/>
+            {/* <img src="/2025/wideimg2.jpg" alt="wide image" className='lg:w-[500px] md:w-32 w-28 flex lg:mx-10 mx-6'/> */}
+          </Marquee>
+    </div>
+  )
+}
+
+export default Carosel
